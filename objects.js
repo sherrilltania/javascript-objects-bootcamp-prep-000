5376fb95c0c6a0980226ad74786de083ca8fe683
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 03:13:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {artistName: 'songTitles'}
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName]: songTitle})
 }
+ function removeFromPlaylist(playlist, artistName){
+   delete playlist.artistName;
+   
+ }
