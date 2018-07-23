cd7bb9860c6f7440b57c382d9a9175219d73539d
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-23 03:19:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ console.log(playlist)
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
-  return playlist
+  playlist
 }
  function removeFromPlaylist(playlist, artistName){
    delete playlist.artistName;
